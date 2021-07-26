2496dd5fd0f2044e9dd9e384a8ce0ad5677dcea6
MariuszMikulec <76539740+MariuszMikulec@users.noreply.github.com>
2021-07-26 21:37:43 +0200

    First step
    Utworzenie początkowego zarysu struktury aplikacji

---
+++ App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <>
+      <header className="App-header" id="main page">
+        <h1>Eden Initiative</h1>
+        <div>
+          <a href="#main page">Strona główna </a>
+          <a>Projekty </a>
+          <a href="#about us">O nas </a>
+          <a href="benefits">Korzyści </a>
+          <a href="#contact us">Kontakt </a>
+
+        </div>
+        <button>Join us!</button>
+
+      </header>
+        <section id="about us">
+          <div>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem dignissimos dolor id nobis officia placeat repudiandae sunt vel vitae?</p>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam iste libero numquam sit suscipit! Ab ipsam officiis sapiente tenetur vel.</p>
+          </div>
+          <div >
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, illo!</p>
+          </div>
+        </section>
+
+        <section id="benefits">
+
+        </section>
+        <footer id="contact us">
+
+        </footer>
+    </>
+  );
+}
+
+export default App;
